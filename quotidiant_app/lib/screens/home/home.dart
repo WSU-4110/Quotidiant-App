44ae6616ac9c4080eb6f62b25ef16e88a70e48bb
Rivan Adib <rivanadib02@gmail.com>
2022-06-11 18:35:19 -0400

    Testing merge

--- quotidiant_app/lib/screens/home/home.dart
+++ quotidiant_app/lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+//Merge test Home Page -R
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
