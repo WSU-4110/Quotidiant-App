4a6681f792decfb04d2163b047e255ec5d7725ad
MatthewP926 <gq3319@wayne.edu>
2022-06-23 14:57:22 -0400

    Added themes that can be used for light and dark mode, includes 4 files.

--- quotidiant_app/lib/screens/settings/settings.dart
+++ quotidiant_app/lib/screens/settings/settings.dart
@@ -1,64 +1,53 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quotidiant_app/main.dart';
+import 'package:quotidiant_app/screens/theme/themebutton.dart';
+import 'package:quotidiant_app/screens/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
 //Matthew Merge Test
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
+  // @override
+  // Widget build(BuildContext context) => Scaffold(
+  //       appBar: AppBar(
+  //         iconTheme: Theme.of(context).iconTheme,
+  //         backgroundColor: Colors.transparent,
+  //         leading: Icon(Icons.menu),
+  //         title: Text(MyApp.title),
+  //         elevation: 0,
+  //         actions: [
+  //           ChangeThemeButtonWidget(),
+  //         ],
+  //       ),
+  //       body: ProfileWidget(),
+  //       extendBody: true,
+  //       bottomNavigationBar: NavigationBarWidget(),
+  //     );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    final text = Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
+        ? 'DarkTheme'
+        : 'LightTheme';
+
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.orange,
+        title: Text(""),
+        actions: [
+          ChangeThemeButtonWidget(),
+        ],
       ),
-      body: Center(child: SwitchScreen()),
-    ));
-  }
-}
-
-class SwitchScreen extends StatefulWidget {
-  @override
-  SwitchClass createState() => new SwitchClass();
-}
-
-class SwitchClass extends State {
-  bool isSwitched = false;
-  var textValue = 'Switch is OFF';
-
-  void toggleSwitch(bool value) {
-    if (isSwitched == false) {
-      setState(() {
-        isSwitched = true;
-        textValue = 'Switch Button is ON';
-      });
-      print('Switch Button is ON');
-    } else {
-      setState(() {
-        isSwitched = false;
-        textValue = 'Switch Button is OFF';
-      });
-      print('Switch Button is OFF');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Transform.scale(
-          scale: 2,
-          child: Switch(
-            onChanged: toggleSwitch,
-            value: isSwitched,
-            activeColor: Colors.blue,
-            activeTrackColor: Colors.yellow,
-            inactiveThumbColor: Colors.redAccent,
-            inactiveTrackColor: Colors.orange,
-          )),
-      Text(
-        '$textValue',
-        style: TextStyle(fontSize: 20),
-      )
-    ]);
+      body: Center(
+        child: Text(
+          'Hello $text!',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
