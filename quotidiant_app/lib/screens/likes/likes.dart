bbaf5d7a91853bfdd96d21903505619a69a77d40
MatthewP926 <gq3319@wayne.edu>
2022-07-12 21:28:49 -0400

    Set up to currently pull the first like from matthew@gmail.com, seperate users need to be setup along with looping to return all the likes

--- quotidiant_app/lib/screens/likes/likes.dart
+++ quotidiant_app/lib/screens/likes/likes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // test merge Mark
 class Likes extends StatelessWidget {
@@ -23,6 +24,39 @@ class Likes extends StatelessWidget {
         ),
       ),
       const Divider(),
+      GetLikes()
     ]));
+  }
+}
+
+class GetLikes extends StatelessWidget {
+  final String documentId = "GAWChSgtDzdZyIhkJYQ8WDhYztn1";
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('Users');
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          List<dynamic> likes = data["likes"];
+          return Text("likes: ${likes[0]}");
+        }
+
+        return Text("loading");
+      },
+    );
   }
 }
